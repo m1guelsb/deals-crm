d8c9d6906f3ce3553fba5a04dbe22410940e0a82
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-07-21 13:36:09 -0300

    [test] add Icon component tests

---
+++ src/components/media/Icon/icon.spec.tsx
@@ -0,0 +1,12 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import { Icon } from "@/components/media";
+import { error } from "@/assets/icons";
+
+describe("Icon component", () => {
+  it("should render correctly", () => {
+    render(<Icon role={"figure"} src={error.src} />);
+
+    expect(screen.getByRole("figure")).toBeInTheDocument();
+  });
+});
