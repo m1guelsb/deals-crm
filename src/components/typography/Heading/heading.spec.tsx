ceef67b2fd761a05a311d72b48379b102ffd6f00
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-07-21 13:36:23 -0300

    [test] add Heading component tests

---
+++ src/components/typography/Heading/heading.spec.tsx
@@ -0,0 +1,11 @@
+import "@testing-library/jest-dom/extend-expect";
+import { render, screen } from "@testing-library/react";
+import { Heading } from "./Heading";
+
+describe("Heading component", () => {
+  it("should render correctly", () => {
+    render(<Heading>Test Heading</Heading>);
+
+    expect(screen.getByText(/Test Heading/i)).toBeInTheDocument();
+  });
+});
