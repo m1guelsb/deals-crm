c325bae718faac0f2c133227b2d2003d4ee65396
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-07-28 20:59:30 -0300

    [feat] add Task component

---
+++ src/components/data-display/Task/Task.tsx
@@ -0,0 +1,78 @@
+import { warning } from "@/assets/icons";
+import { Icon } from "@/components/media";
+import { CSS, styled, theme } from "@/styles/stitches.config";
+import { Task as TaskType } from "@/types/Task";
+import { format, isTomorrow } from "date-fns";
+
+interface TaskProps extends Omit<TaskType, "id"> {
+  css?: CSS;
+}
+
+export const Task = ({ title, due_date, completed, css }: TaskProps) => {
+  const dueDate = new Date(due_date).toLocaleDateString("en-US", {
+    dateStyle: "medium",
+  });
+
+  const isClose = isTomorrow(new Date(dueDate));
+
+  return (
+    <TaskContainer css={css}>
+      <DueDate dueDate={isClose ? "close" : "far"}>{dueDate}</DueDate>
+
+      <Title title={title}>{title}</Title>
+
+      {isClose && (
+        <DueStatus>
+          <Icon
+            src={warning.src}
+            css={{ _iconColor: { fill: theme.colors.error } }}
+          />
+        </DueStatus>
+      )}
+    </TaskContainer>
+  );
+};
+
+const TaskContainer = styled("span", {
+  "height": "2.5rem",
+  "display": "flex",
+  "alignItems": "center",
+  "gap": "0.5rem",
+
+  "_paddingX": "1rem",
+
+  "borderRadius": theme.radii.sm,
+
+  "&:hover": {
+    backgroundColor: theme.colors.background3,
+  },
+});
+
+const DueDate = styled("span", {
+  minWidth: "6.5rem",
+
+  variants: {
+    dueDate: {
+      far: {
+        color: theme.colors.text2,
+      },
+      close: {
+        color: theme.colors.error,
+      },
+    },
+  },
+});
+
+const DueStatus = styled("span", {
+  _alignCenter: true,
+  gap: "0.5rem",
+
+  color: theme.colors.error,
+});
+
+const Title = styled("span", {
+  width: "100%",
+  alignItems: "center",
+  textAlign: "left",
+  _truncate: true,
+});
---
+++ src/types/Task.ts
@@ -0,0 +1,6 @@
+export interface Task {
+  id: number;
+  title: string;
+  due_date: string;
+  completed: boolean;
+}
