625e42ccaed140348b8ea2d54544039dbff85ffa
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-07-28 22:14:41 -0300

    [fix] useReactQuery hook: update prop name

--- src/hooks/query/useReactQuery.ts
+++ src/hooks/query/useReactQuery.ts
@@ -5,7 +5,7 @@ import { useQuery, UseQueryOptions } from "react-query";
 interface useReactQueryProps {
   queryKey: string;
   url: string;
-  configs?: AxiosRequestConfig<any> | undefined;
+  requestConfigs?: AxiosRequestConfig<any> | undefined;
   queryConfigs?: {
     enabled?: boolean;
     refetchOnWindowFocus?: boolean;
@@ -15,13 +15,15 @@ interface useReactQueryProps {
 export function useReactQuery<ResponseDataType>({
   queryKey,
   url,
-  configs,
+  requestConfigs,
   queryConfigs,
 }: useReactQueryProps) {
   const queryResponse = useQuery(
     queryKey,
     async () => {
-      const { data } = await api.get<ResponseDataType>(url, configs);
+      const { data } = await api.get<ResponseDataType>(url, {
+        ...requestConfigs,
+      });
       return data;
     },
     { retry: false, ...queryConfigs }
