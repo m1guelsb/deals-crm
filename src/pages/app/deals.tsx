b780c14ccdcd7e983c0bee23baef1243ee1942c3
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-08-07 09:53:04 -0300

    [feat] add deals page

---
+++ src/pages/app/deals.tsx
@@ -0,0 +1,28 @@
+import Head from "next/head";
+import { NextPage } from "next";
+import { AppLayout } from "@/components/layout";
+import { styled } from "@/styles/stitches.config";
+import { DealsTable } from "@/components/tables";
+
+const Deals: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Deals CMS | Deals</title>
+      </Head>
+
+      <AppLayout sessionTitle={"Deals"}>
+        <DealsContainer>
+          <DealsTable />
+        </DealsContainer>
+      </AppLayout>
+    </>
+  );
+};
+
+export default Deals;
+
+const DealsContainer = styled("section", {
+  height: "100%",
+  padding: "2rem",
+});
