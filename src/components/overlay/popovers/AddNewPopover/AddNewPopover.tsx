ec5603e0db3bf370fcfe42f6b0a9dfe501b74b7a
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-08-08 23:15:13 -0300

    [feat] add BasePopover component

--- src/components/overlay/popovers/AddNewPopover/AddNewPopover.tsx
+++ src/components/overlay/popovers/AddNewPopover/AddNewPopover.tsx
@@ -1,46 +1,70 @@
-import type { ReactNode } from "react";
-import { Root, Trigger, Portal } from "@radix-ui/react-popover";
-import { Arrow, Content } from "./add-new-popover.styles";
+import type { ReactElement } from "react";
+import { Content as PopoverContent } from "@radix-ui/react-popover";
+import { styled, theme } from "@/styles/stitches.config";
+import {
+  BasePopover,
+  Arrow,
+  popoverSlideDown,
+} from "@/components/overlay/popovers/BasePopover";
 import { Button } from "@/components/form";
-import { costumers, deals } from "@/assets/icons";
 import { Icon } from "@/components/media";
-import Link from "next/link";
+import { costumers, deals } from "@/assets/icons";
 
 interface PopoverProps {
-  children: ReactNode;
+  children: ReactElement;
 }
 export const AddNewPopover = ({ children }: PopoverProps) => (
-  <Root>
-    <Trigger style={{ all: "unset" }}>{children}</Trigger>
+  <BasePopover trigger={children}>
+    <Content sideOffset={5}>
+      <Button
+        sType={"secondary"}
+        css={{
+          width: "100%",
+          borderBottomLeftRadius: "0",
+          borderBottomRightRadius: "0",
+        }}
+        rightIcon={<Icon src={deals.src} />}
+      >
+        Deal
+      </Button>
 
-    <Portal>
-      <Content sideOffset={5}>
-        <Button
-          sType={"secondary"}
-          css={{
-            width: "100%",
-            borderBottomLeftRadius: "0",
-            borderBottomRightRadius: "0",
-          }}
-          rightIcon={<Icon src={deals.src} />}
-        >
-          Deal
-        </Button>
+      <Button
+        sType={"secondary"}
+        css={{
+          width: "100%",
+          borderTopLeftRadius: "0",
+          borderTopRightRadius: "0",
+        }}
+        rightIcon={<Icon src={costumers.src} />}
+      >
+        Customer
+      </Button>
 
-        <Button
-          sType={"secondary"}
-          css={{
-            width: "100%",
-            borderTopLeftRadius: "0",
-            borderTopRightRadius: "0",
-          }}
-          rightIcon={<Icon src={costumers.src} />}
-        >
-          Customer
-        </Button>
-
-        <Arrow />
-      </Content>
-    </Portal>
-  </Root>
+      <Arrow />
+    </Content>
+  </BasePopover>
 );
+
+export const Content = styled(PopoverContent, {
+  "height": "fit-content",
+  "width": "10rem",
+
+  "display": "flex",
+  "flexDirection": "column",
+  "alignItems": "stretch",
+
+  "backgroundColor": theme.colors.background3,
+
+  "borderRadius": theme.radii.md,
+  "_border": "All",
+  "borderColor": theme.colors.primary,
+
+  "@media (prefers-reduced-motion: no-preference)": {
+    "animationDuration": "400ms",
+    "animationTimingFunction": "cubic-bezier(0.16, 1, 0.3, 1)",
+    "willChange": "transform, opacity",
+    '&[data-state="open"]': {
+      '&[data-side="bottom"]': { animationName: popoverSlideDown },
+    },
+  },
+});
---
+++ src/components/overlay/popovers/BasePopover/BasePopover.tsx
@@ -0,0 +1,24 @@
+import type { ReactElement, ReactNode } from "react";
+import * as P from "@radix-ui/react-popover";
+import { keyframes, styled, theme } from "@/styles/stitches.config";
+
+interface PopoverProps {
+  trigger: ReactNode;
+  children: ReactElement;
+}
+export const BasePopover = ({ trigger, children }: PopoverProps) => (
+  <P.Root>
+    <P.Trigger style={{ all: "unset" }}>{trigger}</P.Trigger>
+
+    <P.Portal>{children}</P.Portal>
+  </P.Root>
+);
+
+export const Arrow = styled(P.Arrow, {
+  fill: theme.colors.primary,
+});
+
+export const popoverSlideDown = keyframes({
+  "0%": { opacity: 0, transform: "translateY(-5px)" },
+  "100%": { opacity: 1, transform: "translateY(0)" },
+});
