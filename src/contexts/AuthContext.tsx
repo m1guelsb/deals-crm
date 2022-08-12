a64a62c87d817b8c223324f4b6d8daecfcf93c06
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-08-12 01:42:07 -0300

    [refactor] AuthContext: add new useQueryPost hook

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -6,11 +6,12 @@ import {
   useState,
 } from "react";
 import Router from "next/router";
+import { AxiosError } from "axios";
 import { destroyCookie, parseCookies, setCookie } from "nookies";
 import { api } from "@/services/axios";
-import { AxiosError } from "axios";
-import { usePost } from "@/hooks/api/usePost";
+import { useQueryPost } from "@/hooks/api/useQueryPost";
 import { useToast } from "@/hooks/helpers/useToast";
+import { signOut } from "@/utils/functions/signOut";
 import type { User } from "@/types";
 
 type SignInCredentials = {
@@ -21,7 +22,7 @@ type SignInCredentials = {
 interface AuthContextType {
   signIn(credentials: SignInCredentials): void;
   loading: boolean;
-  error: AxiosError<unknown, any> | undefined;
+  error: AxiosError<unknown, any> | null;
   user?: User;
 }
 interface AuthProviderProps {
@@ -34,51 +35,57 @@ export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>();
   const { newToast } = useToast();
 
-  //handle signin post
-  const { loading, error, post } = usePost<
-    { access_token: string },
-    SignInCredentials
-  >({
-    onSuccess: ({ access_token }) => {
-      setCookie(undefined, "deals.access_token", access_token, {
-        maxAge: 60 * 60 * 24 * 30, //30 days
-        path: "/",
-      });
-
-      Router.push("/app");
-
-      api
-        .get<User>("/user")
-        .then(({ data: User }) => setUser(User))
-        .catch(() =>
-          newToast({
-            styleType: "error",
-            title: "An unknown error has occurred",
-            duration: 3000,
-          })
-        );
-    },
-    onError: (error) => {
-      if (error?.response?.status === 401) {
-        newToast({
-          styleType: "error",
-          title: "Wrong credentials!",
-          duration: 3000,
-        });
-      } else
-        newToast({
-          styleType: "error",
-          title: "An unknown error has occurred",
-          duration: 3000,
-        });
-    },
+  const {
+    post: postCredentials,
+    isLoading,
+    error,
+  } = useQueryPost<SignInCredentials, { access_token: string }>({
+    url: "/auth/login",
   });
 
   //signin fn
   const signIn = useCallback(
-    ({ username, password }: SignInCredentials) =>
-      post("/auth/login", { username, password }),
-    [post]
+    ({ username, password }: SignInCredentials) => {
+      postCredentials(
+        { password, username },
+        {
+          onSuccess({ data: { access_token } }) {
+            setCookie(undefined, "deals.access_token", access_token, {
+              maxAge: 60 * 60 * 24 * 30, //30 days
+              path: "/",
+            });
+
+            Router.push("/app");
+
+            api
+              .get<User>("/user")
+              .then(({ data: User }) => setUser(User))
+              .catch(() =>
+                newToast({
+                  styleType: "error",
+                  title: "Error on get user info.",
+                  duration: 3000,
+                })
+              );
+          },
+          onError(error) {
+            if (error?.response?.status === 401) {
+              newToast({
+                styleType: "error",
+                title: "Wrong credentials!",
+                duration: 3000,
+              });
+            } else
+              newToast({
+                styleType: "error",
+                title: "Unexpected error, try again.",
+                duration: 3000,
+              });
+          },
+        }
+      );
+    },
+    [newToast, postCredentials]
   );
 
   //handle get user
@@ -97,8 +104,8 @@ export function AuthProvider({ children }: AuthProviderProps) {
     <AuthContext.Provider
       value={{
         signIn,
-        loading,
-        error,
+        loading: isLoading,
+        error: error,
         user,
       }}
     >
