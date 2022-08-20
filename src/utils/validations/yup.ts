9af368f684151d5d88f27b17623a827a700abba0
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-08-19 21:35:29 -0300

    [fix] deal form validation: adjusts price input min length

--- src/utils/validations/yup.ts
+++ src/utils/validations/yup.ts
@@ -9,7 +9,7 @@ export const signInFormSchema = object().shape({
 export const newDealFormSchema: SchemaOf<DealForm> = object({
   title: string().required("Field required"),
   description: string().required("Field required"),
-  price: string().required("Field required"),
+  price: string().required("Field required").min(2, "Field required"),
   customer: object({
     id: string().required("Select a customer"),
     name: string().required("Select a customer"),
