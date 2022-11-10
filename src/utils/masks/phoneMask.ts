4d5675e1ec20fe14e0c56cc36e4b56e99117e76e
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-10 00:16:28 -0300

    [fix] phoneMask func - adjusts regex mask

--- src/utils/masks/phoneMask.ts
+++ src/utils/masks/phoneMask.ts
@@ -1,9 +1,7 @@
 export const phoneMask = (value: string) => {
   const maskedValue = value
     .replace(/\D/g, "")
-    .replace(/(\d{3})(\d{3})(\d{4})$/, "$1-$2-$3");
-  // .replace(/-(\d{3})$/, "-$1-")
-  // .replace(/-(\d{4})$/, "-$1");
+    .replace(/(\d{3}-?)(\d{3}-?)(\d{4}-?)$/, "$1-$2-$3");
 
   return maskedValue;
 };
