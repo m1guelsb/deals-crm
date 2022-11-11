ba3a586713c72283930aec4b86a4253f9baf537b
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-11 13:14:42 -0300

    [feat] add NewCustomerForm component

--- src/components/form-groups/index.ts
+++ src/components/form-groups/index.ts
@@ -1,4 +1,5 @@
 export * from "./NewDealForm/NewDealForm";
+export * from "./NewCustomerForm/NewCustomerForm";
 export * from "./EditDealForm/EditDealForm";
 export * from "./EditCustomerForm/EditCustomerForm";
 export * from "./NewTaskForm/NewTaskForm";
