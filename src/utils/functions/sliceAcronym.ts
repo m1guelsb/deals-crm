2223b096c9b25e824fe8248136673e920764b53a
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-11 12:25:49 -0300

    [fix] sliceAcronym func - slice only 2 letters and uppercase

--- src/utils/functions/sliceAcronym.ts
+++ src/utils/functions/sliceAcronym.ts
@@ -2,7 +2,9 @@ export const sliceAcronym = (str: string | undefined) => {
   if (!str) return "";
   const acronym = str
     .split(/\s/)
-    .reduce((response, word) => (response += word.slice(0, 1)), "");
+    .reduce((response, word) => (response += word.slice(0, 1)), "")
+    .slice(0, 2)
+    .toUpperCase();
 
   return acronym;
 };
