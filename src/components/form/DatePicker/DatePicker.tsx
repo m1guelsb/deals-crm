735bbaaef08cff7a633c14f3ff32e53c5d5561ca
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-11 13:32:39 -0300

    [feat] add DatePicker component

---
+++ src/components/form/DatePicker/DatePicker.tsx
@@ -0,0 +1,41 @@
+import { Picker } from "./core/Picker";
+import { today, getLocalTimeZone, parseDate } from "@internationalized/date";
+import { useState } from "react";
+import { ErrorMessage } from "../Input/input.styles";
+import { styled } from "@/styles/stitches.config";
+
+interface DatePickerProps {
+  value: any;
+  onChange: (value: string) => void;
+  errorMessage?: string;
+}
+
+export const DatePicker = ({
+  errorMessage,
+  onChange,
+  value,
+}: DatePickerProps) => {
+  const [dateValue, setDateValue] = useState(value);
+
+  const onDatePick = (pickedDate: string) => {
+    setDateValue(pickedDate);
+    onChange(pickedDate);
+  };
+
+  return (
+    <DatePickerContainer>
+      <Picker
+        value={dateValue}
+        onChange={(value: string) => onDatePick(value)}
+        minValue={today(getLocalTimeZone())}
+      />
+      {errorMessage && <ErrorMessage>{errorMessage}</ErrorMessage>}
+    </DatePickerContainer>
+  );
+};
+
+const DatePickerContainer = styled("div", {
+  display: "flex",
+  flexDirection: "column",
+  gap: "0.25rem",
+});
--- src/components/form/index.ts
+++ src/components/form/index.ts
@@ -4,3 +4,4 @@ export * from "./IconButton/IconButton";
 export * from "./LinkButton/LinkButton";
 export * from "./Select/Select";
 export * from "./ComboBox/ComboBox";
+export * from "./DatePicker/DatePicker";
