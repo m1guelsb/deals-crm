ad42330d13c911d1264024203bb7a76e1b70803d
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-12 13:45:28 -0300

    [feat] form groups barrel - add editTaskForm export

--- src/components/form-groups/index.ts
+++ src/components/form-groups/index.ts
@@ -2,4 +2,5 @@ export * from "./NewDealForm/NewDealForm";
 export * from "./NewCustomerForm/NewCustomerForm";
 export * from "./EditDealForm/EditDealForm";
 export * from "./EditCustomerForm/EditCustomerForm";
+export * from "./EditTaskForm/EditTaskForm";
 export * from "./NewTaskForm/NewTaskForm";
