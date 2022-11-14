f0853a1e6fd192ff925514b09f9008e17ebb9b16
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-13 23:22:35 -0300

    [fix] yup deal schema - adjusts customer prop

--- src/utils/validations/yup.ts
+++ src/utils/validations/yup.ts
@@ -10,12 +10,7 @@ export const dealFormSchema: SchemaOf<DealForm> = object({
   title: string().required("Field required"),
   description: string().required("Field required"),
   price: string().required("Field required").min(2, "Field required"),
-
-  customer: object({
-    id: string().required("Select a customer"),
-    name: string().required("Select a customer"),
-  }).required("Select a customer"),
-  customerId: string(),
+  customerId: string().required("Select a customer"),
 
   status: object({
     label: string()
