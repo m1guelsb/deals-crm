da14725325e1a17dde91eddf9e509aac34329936
Miguel S. Barbosa <miguelbarbosacs@gmail.com>
2022-11-20 21:02:36 -0300

    [test] Card - should render

---
+++ src/components/data-display/Card/card.spec.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { getByRole, render } from "@testing-library/react";
+import { Card } from "./Card";
+import { dollar, email } from "@/assets/icons";
+
+describe("Card component", () => {
+  it("should render correctly", () => {
+    const { getByText } = render(
+      <Card title="Card title" value={"Card value"} iconSrc={dollar.src} />
+    );
+
+    const cardEl = getByText("Card title");
+
+    expect(cardEl).toBeInTheDocument();
+  });
+
+});
